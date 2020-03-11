c5c4ce4d09a6713c567e3092cc7b4f6b505aa592
Erik Freundorfer <e.freundorfer@gmail.com>
2020-03-10 21:29:07 -0400

    added ui and page templates

--- archive-education_methods.php
+++ archive-education_methods.php
@@ -18,4 +18,11 @@ $templates = array('pages/education-methods.twig', 'archive.twig', 'index.twig' 
 $context = Timber::context();
 $context['posts'] = new Timber\PostQuery();
 
+$args = array(
+  'posts_per_page' => -1,
+  'post_type' => 'education_methods',
+);
+
+$context['education_methods'] = new Timber\PostQuery($args);
+
 Timber::render( $templates, $context );
---
+++ studies.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Studies
+ *
+ * Methods for TimberHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+Timber::render( 'pages/studies.twig', $context );
