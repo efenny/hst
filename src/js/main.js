8a87f45ab4c2f64f3c64280af66fb2867f9a113d
Erik Freundorfer <e.freundorfer@gmail.com>
2020-03-18 19:52:12 -0400

    finsihed accordions

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,51 @@
-console.log("dank");
+// const toggleSwitch = document.querySelector(
+//   '.theme-switch input[type="checkbox"]'
+// );
 
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+// const currentTheme = localStorage.getItem("theme")
+//   ? localStorage.getItem("theme")
+//   : null;
 
-const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
+// if (currentTheme) {
+//   document.documentElement.setAttribute("data-theme", currentTheme);
 
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+//   if (currentTheme === "dark") {
+//     toggleSwitch.checked = true;
+//   }
+// }
 
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
+// function switchTheme(e) {
+//   if (e.target.checked) {
+//     document.documentElement.setAttribute("data-theme", "dark");
+//     localStorage.setItem("theme", "dark"); //add this
+//   } else {
+//     document.documentElement.setAttribute("data-theme", "light");
+//     localStorage.setItem("theme", "light"); //add this
+//   }
+// }
+
+// toggleSwitch.addEventListener("change", switchTheme, false);
+
+var $ = jQuery.noConflict();
+
+$(document).ready(function() {
+  console.log("Let's hope this all works");
+  accordionsInit();
+});
+
+function accordionsInit() {
+  function accordionFunc(ele) {
+    $(ele + " " + ele + "-inner > .title").click(function(e) {
+      $(this)
+        .closest(ele)
+        .toggleClass("active");
+      $(this).toggleClass("active");
+      $(this)
+        .siblings()
+        .slideToggle();
+
+      e.stopPropagation();
+    });
   }
+  accordionFunc(".accordion");
 }
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light"); //add this
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
