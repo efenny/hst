7c9286488a65af910d1196a7e917008f8229ecb4
Erik Freundorfer <e.freundorfer@gmail.com>
2020-04-17 22:55:43 -0400

    added in video overlay before video starts

--- src/js/main.js
+++ src/js/main.js
@@ -455,3 +455,45 @@ function smooth_scroll() {
       }
     });
 }
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("landing-video", {
+    height: "390",
+    width: "640",
+    videoId: $("#landing-video").attr("data-youtube"),
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo().mute();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+    event.target.playVideo().mute();
+  }
+
+  if (
+    event.data == YT.PlayerState.PLAYING &&
+    event.data != YT.PlayerState.BUFFERING
+  ) {
+    $(".video-cover").hide();
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
